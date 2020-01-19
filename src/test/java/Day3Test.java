242ead1d569acb7d000e53113281ff9abed78ace
Alan Matthews <alan.r.matthews@gmail.com>
2020-01-19 14:13:57 -0500

    Day 3 Part 2

--- src/test/java/Day3Test.java
+++ src/test/java/Day3Test.java
@@ -5,23 +5,44 @@ import static org.junit.Assert.*;
 public class Day3Test {
 
     @Test
-    public void getDistance() {
+    public void getManhattanDistance() {
         String[] Wire1 = new String[]{"R8","U5","L5","D3"};
         String[] Wire2 = new String[]{"U7","R6","D4","L4"};
 
-        assertEquals(Day3.getDistance(Wire1, Wire2), 6);
+        assertEquals(Day3.getManhattanDistance(Wire1, Wire2), 6);
 
         Wire1 = new String[]{"R75","D30","R83","U83","L12","D49","R71","U7","L72"};
         Wire2 = new String[]{"U62","R66","U55","R34","D71","R55","D58","R83"};
 
-        assertEquals(Day3.getDistance(Wire1, Wire2), 159);
+        assertEquals(Day3.getManhattanDistance(Wire1, Wire2), 159);
 
         Wire1 = new String[]{"R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"};
         Wire2 = new String[]{"U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"};
 
-        assertEquals(Day3.getDistance(Wire1, Wire2), 135);
+        assertEquals(Day3.getManhattanDistance(Wire1, Wire2), 135);
 
         // Part 1 of the actual problem:
-        assertEquals(Day3.getDistance(Day3.Wire1, Day3.Wire2), 217);
+        assertEquals(Day3.getManhattanDistance(Day3.Wire1, Day3.Wire2), 217);
+    }
+
+    @Test
+    public void getWireDistance() {
+        String[] Wire1 = new String[]{"R8","U5","L5","D3"};
+        String[] Wire2 = new String[]{"U7","R6","D4","L4"};
+
+        assertEquals(Day3.getWireDistance(Wire1, Wire2), 30);
+
+        Wire1 = new String[]{"R75","D30","R83","U83","L12","D49","R71","U7","L72"};
+        Wire2 = new String[]{"U62","R66","U55","R34","D71","R55","D58","R83"};
+
+        assertEquals(Day3.getWireDistance(Wire1, Wire2), 610);
+
+        Wire1 = new String[]{"R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"};
+        Wire2 = new String[]{"U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"};
+
+        assertEquals(Day3.getWireDistance(Wire1, Wire2), 410);
+
+        // Part 1 of the actual problem:
+        assertEquals(Day3.getWireDistance(Day3.Wire1, Day3.Wire2), 3454);
     }
 }
